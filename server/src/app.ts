cf0ee3e0d7c5579edc5880228747178e9cc6779d
Ajay <ajaykumawat9904@gmail.com>
2024-09-13 11:45:44 +0530

    feat(graphQL): connect with express

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,12 +1,43 @@
-import dotenv, { populate } from "dotenv";
+import express from "express";
+import cors from "cors";
+import { errorMiddleware } from "./middlewares/error.js";
+import morgan from "morgan";
+import dotenv from "dotenv";
 import { connectdb } from "./database/db.js";
 import { connectGraphQL } from "./graphql/graphql.js";
-dotenv.config({ path: "./.env" });
+import { expressMiddleware } from "@apollo/server/express4"
 
+dotenv.config({ path: "./.env" });
 export const envMode = process.env.NODE_ENV?.trim() || "DEVELOPMENT";
 const port = Number(process.env.PORT) || 3000;
 
 const mongoURI = process.env.MONGO_URI!;
 connectdb(mongoURI);
 
-connectGraphQL(port, envMode);
+const server = connectGraphQL();
+await server.start()
+
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors({ origin: " * ", credentials: true }));
+app.use(morgan("dev"));
+app.use(errorMiddleware);
+
+app.use("/graphql", expressMiddleware(server));
+
+app.get("/", (req, res) => {
+  res.send("Hello, World!");
+});
+
+
+app.get("*", (req, res) => {
+  res.status(404).json({
+    success: false,
+    message: "Page not found",
+  });
+});
+
+app.listen(port, () =>
+  console.log("Server is working on Port:" + port + " in " + envMode + " Mode.")
+);
--- server/src/graphql/graphql.ts
+++ server/src/graphql/graphql.ts
@@ -1,23 +1,24 @@
 import { ApolloServer } from "@apollo/server";
 import { graphQlSchema } from "./schema/schema.js";
 import { GraphQlResolver } from "./resolver/resolver.js";
-import { startStandaloneServer } from "@apollo/server/standalone";
 
-export const connectGraphQL = (port: number, envMode: string) => {
+export const connectGraphQL = () => {
   const server = new ApolloServer({
     typeDefs: graphQlSchema,
     resolvers: GraphQlResolver,
   });
 
-  startStandaloneServer(server, {
-    listen: {
-      port,
-    },
-  })
-    .then(() => {
-      console.log(`Server is working on Port: ${port} in ${envMode} Mode.`);
-    })
-    .catch((error) => {
-      console.error(error);
-    });
+  return server;
+
+  // startStandaloneServer(server, {
+  //   listen: {
+  //     port,
+  //   },
+  // })
+  //   .then(() => {
+  //     console.log(`Server is working on Port: ${port} in ${envMode} Mode.`);
+  //   })
+  //   .catch((error) => {
+  //     console.error(error);
+  //   });
 };
